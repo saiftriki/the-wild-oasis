3301b3c5daeddda49926bfbc533fcce241388be2
saiftriki <saiftriki17@gmail.com>
2025-09-04 19:37:46 +0100

    Update loginForm

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -4,7 +4,6 @@ import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import SpinnerMini from "../../ui/SpinnerMini";
 import FormRowVertical from "../../ui/FormRowVertical";
-import { login } from "../../services/apiAuth";
 import { useLogin } from "./uselogin";
 
 function LoginForm() {
