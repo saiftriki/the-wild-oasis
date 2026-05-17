eec0a217332e85d6a1de13cb8313b7e9f5164c65
Saifeddine Triki <126980135+saiftriki@users.noreply.github.com>
2026-05-17 22:40:38 +0100

    update login form values

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("test@test.com");
+  const [password, setPassword] = useState("test123");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
